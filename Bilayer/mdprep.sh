0f49af98859f10b2f44c4aebb095504b2d10ae79
Alex Yang <ahy3nz@master.cl.vanderbilt.edu>
2017-01-17 21:48:50 -0600

    optimized mdprep on rahman

--- Bilayer/mdprep.sh
+++ Bilayer/mdprep.sh
@@ -2,17 +2,17 @@
 #grompp EM0
 gmx grompp -f em0.mdp -c $1.gro -p $1.top -o em0_$1.tpr -maxwarn 1 > em0_grompp_$1.log 2>&1
 #EM0
-gmx mdrun -deffnm em0_$1 -nt 1
+gmx mdrun -ntomp 1 -deffnm em0_$1 -nt 1
 
 #grompp EM1
 gmx grompp -f em1.mdp -c em0_$1.gro -p $1.top -o em1_$1.tpr -maxwarn 1 > em1_grompp_$1.log 2>&1
 #EM1
-gmx mdrun -deffnm em1_$1 -nt 1
+gmx mdrun -ntomp 1 -deffnm em1_$1 -nt 1
 
 #grompp EM2
 gmx grompp -f em2.mdp -c em1_$1.gro -p $1.top -o em2_$1.tpr -maxwarn 1 > em2_grompp_$1.log 2>&1
 #EM2
-gmx mdrun -deffnm em2_$1 -nt 1
+gmx mdrun -ntomp 1 -deffnm em2_$1 -nt 1
 
 #Edit topology to include charges
 python UpdateTopolCharge.py -p $1.top
@@ -20,13 +20,13 @@ python UpdateTopolCharge.py -p $1.top
 #grompp EM3
 gmx grompp -f em3.mdp -c em2_$1.gro -p $1.top -o em3_$1.tpr > em3_grompp_$1.log 2>&1
 #EM3
-gmx mdrun -deffnm em3_$1 -nt 1
+gmx mdrun -ntomp 1 -deffnm em3_$1 -nt 1
 
 #grompp NVT eq 
 gmx grompp -f nvt.mdp -c em3_$1.gro -p $1.top -o nvteq_$1.tpr > nvteq_grompp_$1.log 2>&1
 
 #NVT Eq
-gmx mdrun -deffnm nvteq_$1 
+gmx mdrun -ntomp 1 -deffnm nvteq_$1 
 
 #grompp NPT md
 #gmx grompp -f md.mdp -c nvteq_$1.gro -p $1.top -o md_$1.tpr  > md_grompp_$1.log 2>&1
