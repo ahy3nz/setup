d147fcf9463f7d1ebcff588102eeadb52c382fbd
Alex Yang <ahy3nz@filey.vuse.vanderbilt.edu>
2017-03-01 17:55:18 -0600

    ordered layer creation

--- CoarseGraining/CG_bilayersetup_mbuild.py
+++ CoarseGraining/CG_bilayersetup_mbuild.py
@@ -4,11 +4,62 @@ import pdb
 import numpy as np
 import mdtraj as mdtraj
 import sys
+from itertools import product
 from optparse import OptionParser
 from Prototypes_CG import *
 
+def new_make_layer(n_x = 8, n_y = 8, lipid_system_info = None, tilt_angle = 0, spacing = 0, layer_shift = 0, res_index = 0, table_of_contents = None,
+        random_z_displacement = 0, top_file = None):
+
+    layer = mb.Compound()
+
+    # Create ordered pairs
+    ordered_pairs = []
+    for i, j in product(range(n_x), range(n_y)):
+        ordered_pairs.append((i,j))
+
+
+    # Randomly assign ordered pairs to each lipid
+    # based on the way lipids is set, all of one molecule is listed first
+    # before getting to the next one
+    # Loop through each type of molecule (DSPC, DPPC, etc.)
+    for i, lipid_type in enumerate(lipid_system_info):
+        n_molecule_per_leaflet = int(lipid_type[1]/2)
+        # Loop through the system's quantity of that particular molecule
+        for n in range(n_molecule_per_leaflet):
+            random_index = np.random.randint(0, len(ordered_pairs))
+            (i,j) = ordered_pairs.pop(random_index)
+            #lipid_molecule = lipid_type[0]
+            #lipid_offset = lipid_type[2]
+            # do geometry transformations
+            molecule_to_add = mb.clone(lipid_type[0])
+            # Apply tilt angle
+            mb.spin_y(molecule_to_add, tilt_angle)
+
+            # Apply z_offset
+            z_offset = lipid_type[2]
+
+            # Apply APL and z_offset to identify the position for the molecule in the grid
+            position = [i * spacing, j * spacing, z_offset + 
+                    (-1 * np.random.random() * random_z_displacement)]
+            mb.translate(molecule_to_add, position)
+
+            # Add the new molecule to the layer
+            layer.add(molecule_to_add)
+            
+            # Add to table of contents
+            table_of_contents.write("{:<10d}{:<10s}{:<10d}\n".format(res_index, molecule_to_add.name, molecule_to_add.n_particles))
+            res_index += 1
+        #Add to top file
+        top_file.write("{:<10s}{:<10d}\n".format(molecule_to_add.name, n_molecule_per_leaflet))
+
+
+    return layer, res_index
+
+
+
 def make_layer(n_x = 8, n_y = 8, lipids = None, tilt_angle = 0, spacing = 0, layer_shift = 0, res_index = 0, table_of_contents = None,
-        random_z_displacement = 0):
+        random_z_displacement = 0, top_file = None):
     """ Generate a bilayer leaflet by randomly laying down leaflets in a 2D grid
 
     Parameters
@@ -65,11 +116,14 @@ def make_layer(n_x = 8, n_y = 8, lipids = None, tilt_angle = 0, spacing = 0, lay
             # Add to table of contents
             table_of_contents.write("{:<10d}{:<10s}{:<10d}\n".format(res_index, molecule_i[0].name, molecule_i[0].n_particles))
             res_index += 1
+            
+            #Add to top file
+            top_file.write("{:<10s}{:<10d}\n".format(molecule_i[0].name, 1))
     # Apply final layer shifts
     mb.translate(layer, [0, 0, layer_shift])
     #mb.translate(water_layer, [0, 0, layer_shift])
     #return layer, res_index, water_layer
-    return layer, res_index
+    return layer, res_index, top_file
 
 
 """
@@ -126,7 +180,7 @@ def enumerate_system_components(lipid_system_info = None):
     return top_lipids, bot_lipids
 
 
-def write_top_file(filename = 'default', lipid_system_info = None, n_solvent = 0):
+def write_top_file_header(filename = 'default', lipid_system_info = None, n_solvent = 0):
     """ Generate topology file
 
     Parameters
@@ -142,23 +196,30 @@ def write_top_file(filename = 'default', lipid_system_info = None, n_solvent = 0
 
     """
 
-    top_file = open(filename + '.top', 'w')
+    top_file = open(filename + '2.top', 'w')
 
     # Include statment, edit for path to maritini FF
     top_file.write("#include \"/Users/ahy3nz/Programs/setup/FF/CG/martini_ff.itp\" \n")
     top_file.write("\n[ system ]\n")
     top_file.write("Coarse-grained bilayer system\n")
     top_file.write("\n[ molecules ] \n") 
+    return top_file
 
     # Using lipid system information, iterate through each molecule, printing out name and number of molecules 
-    for i, lipid_type in enumerate(lipid_system_info):
-        n_molecule = int(lipid_type[1])
-        molecule_name = lipid_type[0].name
-        if n_molecule != 0:
-            top_file.write("{:<10s}{:<10d}\n".format(molecule_name, n_molecule))
+    #for i, lipid_type in enumerate(lipid_system_info):
+    #    n_molecule = int(lipid_type[1])
+    #    molecule_name = lipid_type[0].name
+    #    if n_molecule != 0:
+    #        top_file.write("{:<10s}{:<10d}\n".format(molecule_name, n_molecule))
 
-    # Write out waters
+    ## Write out waters
+    #top_file.write("{:<10s}{:<10d}\n".format(water().name, n_solvent))
+
+
+def write_top_file_footer(top_file = None, lipid_system_info = None, n_solvent = 0):
+    # Just write out the number of solvents we have
     top_file.write("{:<10s}{:<10d}\n".format(water().name, n_solvent))
+    return top_file
 
 def solvate_bilayer(system = None, n_x = 8, n_y = 8, n_solvent_per_lipid = 5, water_spacing = 0.8, 
         res_index = 0, table_of_contents = None):
@@ -288,13 +349,17 @@ if len(bot_lipids)!= (n_lipid)/2:
 table_of_contents = open(filename+'.dat', 'w')
 res_index = 1
 
+# Write topology file
+print("Writing <{0}.top> ...".format(filename))
+top_file = write_top_file_header(filename = filename, lipid_system_info = lipid_system_info, n_solvent = n_solvent)
+
 # Generate bottom layer randomly
-bot_layer, res_index = make_layer(n_x = 8, n_y = 8, lipids = bot_lipids, tilt_angle = tilt_angle, spacing = spacing, layer_shift = 0,
-        res_index = res_index, table_of_contents = table_of_contents, random_z_displacement = random_z_displacement)
+bot_layer, res_index  = new_make_layer(n_x = 8, n_y = 8, lipid_system_info = lipid_system_info, tilt_angle = tilt_angle, spacing = spacing, layer_shift = 0,
+        res_index = res_index, table_of_contents = table_of_contents, random_z_displacement = random_z_displacement, top_file = top_file)
 
 # Generate the top layer randomly
-top_layer, res_index = make_layer(n_x = 8, n_y = 8, lipids = top_lipids, tilt_angle = tilt_angle, spacing = spacing, layer_shift = 3.2,
-        res_index = res_index, table_of_contents = table_of_contents, random_z_displacement = random_z_displacement)
+top_layer, res_index = new_make_layer(n_x = 8, n_y = 8, lipid_system_info = lipid_system_info, tilt_angle = tilt_angle, spacing = spacing, layer_shift = 3.2,
+        res_index = res_index, table_of_contents = table_of_contents, random_z_displacement = random_z_displacement, top_file = top_file)
        
 # Rotate bottom layer to form bilayer
 mb.spin_y(bot_layer, theta=np.pi)
@@ -307,7 +372,7 @@ system.add(top_layer)
 # Solvate system
 system = solvate_bilayer(system = system, n_x = n_x, n_y = n_y, n_solvent_per_lipid = n_solvent_per_lipid, 
         res_index = res_index, table_of_contents = table_of_contents)
-
+top_file = write_top_file_footer(top_file = top_file, lipid_system_info = lipid_system_info, n_solvent = n_solvent)
 
 # Shift everything to positive z
 min_z_shift = min(system.xyz[:,2])
@@ -320,7 +385,5 @@ with warnings.catch_warnings():
     system.save(filename + '.gro',overwrite=True)
 table_of_contents.close()
 
-# Write topology file
-print("Writing <{0}.top> ...".format(filename))
-write_top_file(filename = filename, lipid_system_info = lipid_system_info, n_solvent = n_solvent)
+
 
