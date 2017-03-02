fd54d3a1f18f78cafaa9694b4c77cde03289d2ea
Alex Yang <ahy3nz@filey.vuse.vanderbilt.edu>
2017-03-01 18:24:52 -0600

    Water box now defined, ordered layer creation

--- CoarseGraining/Prototypes_CG/water.py
+++
@@ -1,7 +0,0 @@
-import mbuild as mb
-import os
-class water(mb.Compound):
-    def __init__(self):
-        super(water, self).__init__()
-        mb.load(os.getcwd()+'/Prototypes_CG/water.gro', compound=self)
-
