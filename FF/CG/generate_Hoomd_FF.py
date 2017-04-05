bbdcddc00f784b7f802ccb7386bdfbad36cc2624
ahy3nz <alexander.h.yang@vanderbilt.edu>
2017-04-05 16:44:09 -0500

    NonBonded forces, fix capitalization

--- FF/CG/generate_Hoomd_FF.py
+++ FF/CG/generate_Hoomd_FF.py
@@ -8,20 +8,16 @@ atomtypes = ['P5' , 'P4' , 'BP4' , 'SP4' , 'P3'	, 'P2', 'P1','SP1' ,
              'C5', 'SC5', 'C4', 'SC4', 'C3'	, 'SC3'	, 'C2', 'AC2', 'SC2', 'C1', 'AC1', 'SC1',
              'Qda', 'Qd', 'Qa', 'Q0']
 outfile = open('xmlstuff.xml','w')
-outfile.write("<Forcefield>\n")
+outfile.write("<ForceField>\n")
 
 # Atomtype xml writing
-outfile.write("<Atomtypes>\n")
+outfile.write("<AtomTypes>\n")
 for x in atomtypes:
     # Exception is Q0(charge +1.0) and Qa(charge -1.0)
-    if "Q0" == x:
-        xml_line="\t<Type name=\"Q0\" class=\"Q0\" element=\"_Q0\" mass=\"72\" charge=\"1.0\" def=\"[_Q0]\" desc=\"PC head\"/>"
-    elif "Qa" == x:
-        xml_line="\t<Type name=\"Qa\" class=\"Qa\" element=\"_Qa\" mass=\"72\" charge=\"-1.0\" def=\"[_Qa]\" desc=\"PC head\"/>"
-    else:
-        xml_line = "\t<Type name=\"{0}\" class=\"{0}\" element=\"_{0}\" mass=\"72\" def=\"[_{0}]\"/>".format(x)
+    # But this should be handled in printing the Nonbonded Forces
+    xml_line = "\t<Type name=\"{0}\" class=\"{0}\" element=\"_{0}\" mass=\"72\" def=\"[_{0}]\"/>".format(x)
     outfile.write(xml_line+"\n")
-outfile.write("</Atomtypes>\n")
+outfile.write("</AtomTypes>\n")
 
 # Bond xml writing
 outfile.write("<HarmonicBondForce>\n")
@@ -50,7 +46,8 @@ for x,z in itertools.combinations_with_replacement(atomtypes,2):
             xml_line = "\t<Angle class1=\"{}\" class2=\"{}\" class3=\"{}\" angle=\"1\" k=\"1\"/>".format(x,y,z)
         outfile.write(xml_line+"\n")
 #<Angle class1="tail" class2="mhead2" class3="oh2" angle="7" k="7"/>
+# TODO: add nonbonded force stuff
 outfile.write("</HarmonicAngleForce>\n")
-outfile.write("</Forcefield>\n")
+outfile.write("</ForceField>\n")
 outfile.close()
 
