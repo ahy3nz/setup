c1d6a767595d25c8746ee3d2339da60ed69e94dc
ahy3nz <alexander.h.yang@vanderbilt.edu>
2017-04-14 09:36:38 -0500

    acd24 prototypes

--- Bilayer/Prototypes/__init__.py
+++ Bilayer/Prototypes/__init__.py
@@ -7,9 +7,10 @@ from Prototypes.alc20 import *
 from Prototypes.acd22 import *
 from Prototypes.alc22 import *
 from Prototypes.alc24 import *
+from Prototypes.acd24 import *
 from Prototypes.CHOL import *
 from Prototypes.DPPC import *
 from Prototypes.DSPC import *
 from Prototypes.ISIS import *
 from Prototypes.H2O import *
-__all__ = ["alc12", "alc14", "acd16", "alc16", "alc18", "alc20", "acd22", "alc22", "alc24", "CHOL", "DPPC", "DSPC", "ISIS", "H2O"]
+__all__ = ["alc12", "alc14", "acd16", "alc16", "alc18", "alc20", "acd22", "alc22", "alc24", "acd24", "CHOL", "DPPC", "DSPC", "ISIS", "H2O"]
