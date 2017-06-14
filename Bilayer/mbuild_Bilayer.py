1394772da8a218c65508bb8e3ea3c11903a3087d
ahy3nz <alexander.h.yang@vanderbilt.edu>
2017-06-14 13:28:38 -0500

    Eliminated use of table of contents

--- Bilayer/mbuild_Bilayer.py
+++ Bilayer/mbuild_Bilayer.py
@@ -11,6 +11,8 @@ from Prototypes import *
 from scriptWriter import *
 
 GMX_FF_DIR = "/raid6/homes/ahy3nz/Programs/setup/FF/gromos53a6/"
+residues = set()
+
 
 def new_make_layer(n_x = 8, n_y = 8, lipid_system_info = None, tilt_angle = 0, spacing = 0, 
         layer_shift = 0, res_index = 0, table_of_contents = None,
@@ -91,10 +93,10 @@ def new_make_layer(n_x = 8, n_y = 8, lipid_system_info = None, tilt_angle = 0, s
 
             # Add the new molecule to the layer
             layer.add(molecule_to_add)
-                
-            # Add to table of contents
-            table_of_contents.write("{:<10d}{:<10s}{:<10d}\n".format(res_index, molecule_to_add.name, molecule_to_add.n_particles))
 
+            # Add to list of residue strings
+            residues.add(molecule_to_add.name)
+                
             # Add to lipid dictionary
             if molecule_to_add.name in lipid_atom_dict:
                 lipid_atom_dict[molecule_to_add.name] +=list(range(atom_index, atom_index + molecule_to_add.n_particles, 1))
@@ -102,7 +104,6 @@ def new_make_layer(n_x = 8, n_y = 8, lipid_system_info = None, tilt_angle = 0, s
                 lipid_atom_dict[molecule_to_add.name] = list(range(atom_index, atom_index + molecule_to_add.n_particles,1))
 
             # Increment counters
-            res_index += 1
             atom_index += molecule_to_add.n_particles
         
 
@@ -221,11 +222,12 @@ def solvate_bilayer(system = None, n_x = 8, n_y = 8, n_solvent_per_lipid = 5, wa
     n_water_y = int(np.floor(width/water_spacing))
     n_water_z = int(np.ceil(n_solvent_leaflet / (n_water_x * n_water_y)))
     height = n_water_z * water_spacing
+    residues.add(HOH().name)
     #cube = mb.Grid3DPattern(n_x, n_y, n_solvent_per_lipid)
     #cube.scale( [ water_spacing * n_x, water_spacing * n_y, water_spacing * n_solvent_per_lipid])
     cube = mb.Grid3DPattern(n_water_x, n_water_y, n_water_z)
     cube.scale([length, width, height])
-    bot_water_list = cube.apply(H2O())
+    bot_water_list = cube.apply(HOH())
     bot_water_list = bot_water_list[ : n_solvent_leaflet]
     bot_water = mb.Compound()
     for compound in bot_water_list:
@@ -236,9 +238,6 @@ def solvate_bilayer(system = None, n_x = 8, n_y = 8, n_solvent_per_lipid = 5, wa
     shift_botwater = abs(highest_botwater - lowest_botlipid) + 0.1
     bot_water.translate( [0, 0, -1 * shift_botwater])
     # Add waters to table of contents
-    for i in range(n_x * n_y * n_solvent_per_lipid):
-        table_of_contents.write("{:<10d}{:<10s}{:<10d}\n".format(res_index, "HOH", H2O().n_particles))
-        res_index += 1 
     
     # Construct 3D grid of water
     # Compute distances to translate such that water is either below or above bilayer
@@ -247,7 +246,7 @@ def solvate_bilayer(system = None, n_x = 8, n_y = 8, n_solvent_per_lipid = 5, wa
    # cube.scale( [ water_spacing * n_x, water_spacing * n_y, water_spacing * n_solvent_per_lipid])
     cube = mb.Grid3DPattern(n_water_x, n_water_y, n_water_z)
     cube.scale([length, width, height])
-    top_water_list = cube.apply(H2O())
+    top_water_list = cube.apply(HOH())
     top_water_list = top_water_list[ : n_solvent_leaflet]
     top_water = mb.Compound()
     for compound in top_water_list:
@@ -258,9 +257,6 @@ def solvate_bilayer(system = None, n_x = 8, n_y = 8, n_solvent_per_lipid = 5, wa
     shift_topwater = abs(highest_toplipid - lowest_topwater) + 0.1
     top_water.translate([0,0, shift_topwater])
     # Add waters to table of contents
-    for i in range(n_x * n_y * n_solvent_per_lipid):
-        table_of_contents.write("{:<10d}{:<10s}{:<10d}\n".format(res_index, "HOH", H2O().n_particles))
-        res_index += 1
 
     system.add(bot_water)
     system.add(top_water)
@@ -270,24 +266,10 @@ def solvate_bilayer(system = None, n_x = 8, n_y = 8, n_solvent_per_lipid = 5, wa
 
     
     # Add waters to lipid_atom_dict
-    lipid_atom_dict['SOL'] = list(range(atom_index, atom_index + (2 * n_x * n_y * n_solvent_per_lipid * H2O().n_particles), 1))
-    atom_index += 2 * n_x * n_y * n_solvent_per_lipid * H2O().n_particles
+    lipid_atom_dict['SOL'] = list(range(atom_index, atom_index + (2 * n_x * n_y * n_solvent_per_lipid * HOH().n_particles), 1))
+    atom_index += 2 * n_x * n_y * n_solvent_per_lipid * HOH().n_particles
     return system, waterbox, lipid_atom_dict, atom_index
 
-def write_toc_file_box(table_of_contents = None, box = None):
-    """ Write box information to table of contents file
-
-    Parameters
-    ----------
-    table_of_contents : file
-        Output file listing residue index, residue name, n_particles for tha residue
-
-    box : mb.Box()
-        System box
-
-
-    """
-    table_of_contents.write("{:<8.3f}{:<8.3f}{:<8.3f}\n".format(box.maxs[0], box.maxs[1], box.maxs[2]))
 
 def write_ndx_file(filename = None, lipid_atom_dict = None):
     """ Write gromacs index file
@@ -334,131 +316,6 @@ def write_ndx_file(filename = None, lipid_atom_dict = None):
     ndx_file.write(system_string+"\n")
     ndx_file.close()
 
-def split_lines(source = None, toc = False):
-    """ Simple script to take a file.readlines() and split each line into a list
-
-    Parameters
-    ----------
-    source : list
-        A file whose lines have been read into a list
-    toc : Boolean
-        True if the file-to-be-read is a table of contents file
-
-    Returns
-    -------
-    destination : list
-        A list whose entries are lists of the split lines
-    """
-    destination = []
-    last_index = len(source)
-    if toc:
-        for i, line in enumerate(source):
-            destination.append(line.split())
-    else:
-        for i, line in enumerate(source):
-            if i <= 1 or i == last_index: # For header and foote reading
-                destination.append(line.split())
-            else:
-                gro_entry = []
-                gro_entry.append(line[0:5])
-                gro_entry.append(line[5:10])
-                gro_entry.append(line[10:15])
-                gro_entry.append(line[15:20])
-                gro_entry.append(line[20:28])
-                gro_entry.append(line[28:36])
-                gro_entry.append(line[36:44])
-
-                destination.append(gro_entry)
-    return destination
-
-def gather_header(old_gro_lines = None, new_gro_file = None):
-    """ Write gro file header information
-
-    Parameters
-    ---------
-    old_gro_lines : list
-        List whose entries are split lines of gro file
-    new_gro_file : file
-        Output gro file
-
-    Returns
-    -------
-    old_gro_lines : list
-        List whose entries are split lines of gro file, with updated lines removed
-
-        """
-    line_index = 0
-    while (len(old_gro_lines[0]) != 7 or line_index < 2):
-        #pdb.set_trace()
-        new_gro_file.write(" ".join(old_gro_lines.pop(0)) + "\n")
-        line_index += 1
-    return old_gro_lines
-
-def gather_body(old_gro_lines = None, table_of_contents = None, new_gro_file = None):
-    """ Write body of the topology file
-
-    Parameters
-    ----------
-    old_gro_lines : list
-        List whose entries are split lines of gro file
-    table_of_contents : file
-        Input file listing residue index, residue name, n_particles for tha residue
-    new_gro_file : file
-        Output gro file
-
-    Returns
-    -------
-    old_gro_lines : list
-        List whose entries are split lines of gro file, with updated lines removed
-
-    Notes
-    -----
-    Gro file format
-    {:>5.0f}{:5s}{:>5s}{:>5s}{:8.3f}{:8.3f}{:8.3f}
-    resindex(1 indexed), resname, atomtype, atomindex(1 indexed), x-coord, y-coord, z-coord
-
-    """
-    # Iterate through the table of contents, which then tells the inner loop how many times to iterate
-    for i in range(len(table_of_contents)-1):
-        entry = table_of_contents[i]
-        res_index = entry[0]
-        res_name = entry[1]
-        res_atoms = int(entry[2])
-        for j in range(res_atoms):
-            
-            old_line = old_gro_lines.pop(0)
-            if "HOH" in res_name and j==0:
-                new_line = (float(res_index), res_name, 'OW', old_line[3], float(old_line[4]),
-                    float(old_line[5]), float(old_line[6]))
-            elif "HOH" in res_name and j==1:
-                new_line = (float(res_index), res_name, 'HW1', old_line[3], float(old_line[4]),
-                    float(old_line[5]), float(old_line[6]))
-            elif "HOH" in res_name and j==2:
-                new_line = (float(res_index), res_name, 'HW2', old_line[3], float(old_line[4]),
-                    float(old_line[5]), float(old_line[6]))
-            else:
-                new_line = (float(res_index), res_name, old_line[2], old_line[3], float(old_line[4]),
-                    float(old_line[5]), float(old_line[6]))
-
-
-            new_gro_file.write("{:>5.0f}{:5s}{:>5s}{:>5s}{:8.3f}{:8.3f}{:8.3f}\n".format(new_line[0], new_line[1],
-                new_line[2], new_line[3], new_line[4], new_line[5], new_line[6]))
-    return old_gro_lines
-
-def gather_footer(table_of_contents= None, new_gro_file = None):
-    """ Print footer information (box size)
-
-    Parameters
-    ----------
-    table_of_contents : file
-        Input file listing residue index, residue name, n_particles for tha residue
-    new_gro_file : file
-        Output gro file
-
-    """
-    #new_gro_file.write(' '.join(table_of_contents[-1])+"\n")
-    new_gro_file.write('{:>10.5f}{:>10.5f}{:>10.5f}\n'.format(float(table_of_contents[-1][0]), float(table_of_contents[-1][1]), 
-        float(table_of_contents[-1][2])))
 
 parser = OptionParser()
 parser.add_option("-f", action="store", type="string", default = "CG_bilayer", dest = "filename")
@@ -509,24 +366,6 @@ n_solvent = n_lipid * n_solvent_per_lipid
 random_z_displacement = 0.05
 
 
-# For absolute numbers
-'''
-if options.explicit:
-    lipid_system_info = [(DSPC(), options.DSPC_frac, 0.0),
-                          (DPPC(), options.DPPC_frac, -0.3),
-                          (acd16(), options.acd16_frac, -1.2),
-                          (acd22(), options.acd22_frac, -1.1),
-                          (alc12(), options.alc12_frac, -1.2),
-                          (alc14(), options.alc14_frac, -1.2),
-                          (alc16(), options.alc16_frac, -1.2),
-                          (alc18(), options.alc18_frac, -1.2),
-                          (alc20(), options.alc20_frac, -1.2),
-                          (alc22(), options.alc22_frac, -1.1),
-                          (alc24(), options.alc24_frac, -1.0),
-                          (acd24(), options.alc24_frac, -1.0),
-                          (ISIS(), options.ISIS_frac, -2.5),
-                          (CHOL(), options.CHOL_frac, -0.8)] 
-                          '''
 if options.explicit:
     n_lipid = 1
 
@@ -558,8 +397,6 @@ if(sum([lipid[1] for lipid in lipid_system_info]) != n_lipid):
     sys.exit("System setup error: number of components does not match layer size")
 
 lipid_atom_dict = OrderedDict()
-# Table of contents to externally store residue indices and names for post setup
-table_of_contents = open(filename+'.dat', 'w')
 res_index = 1
 atom_index = 1
 
@@ -608,39 +445,5 @@ min_z_shift = min(system.xyz[:,2])
 system.translate( [0.1, 0.1, -1 * min_z_shift])
 box.maxs[2] += -1*min_z_shift
 
-# Write to table of contents
-write_toc_file_box(table_of_contents = table_of_contents, box = box)
-table_of_contents.close()
-
-
-# Write gro file suppressing mbuild warnings
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    system.save(filename + '.gro', box =box,overwrite=True)
-
-# Write to an index file
-write_ndx_file(filename = filename, lipid_atom_dict = lipid_atom_dict)
-
-# Use table of contents to update the gro file
-table_of_contents_file = open(filename + '.dat', 'r').readlines()
-old_gro_file = open(filename + '.gro', 'r').readlines()
-new_gro_file = open(filename + '.gro', 'w')
-print("Updating <{0}.gro> ...".format(filename))
-
-# Split each line in the old gro file
-old_gro_lines = split_lines(old_gro_file, toc = False)
-
-# Split each line in the table of contents file
-table_of_contents = split_lines(table_of_contents_file, toc=True)
-
-# Header information
-old_gro_lines = gather_header(old_gro_lines = old_gro_lines, new_gro_file = new_gro_file)
-        
-# Body information 
-old_gro_lines = gather_body(old_gro_lines = old_gro_lines, 
-        table_of_contents = table_of_contents, new_gro_file = new_gro_file)
-
-# Footer information
-gather_footer(table_of_contents = table_of_contents, new_gro_file = new_gro_file)
-
-
+# Write gro file 
+system.save(filename + '.gro', box =box,overwrite=True, residues = list(residues))
