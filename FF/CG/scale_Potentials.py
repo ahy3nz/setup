64c3a88e3c307017be0068001fbc51cafdf9f6aa
ahy3nz <alexander.h.yang@vanderbilt.edu>
2017-07-20 17:14:25 -0500

    Fix error reporring

--- FF/CG/scale_Potentials.py
+++ FF/CG/scale_Potentials.py
@@ -13,7 +13,7 @@ parser.add_option("--input", action = "store", type = "string", dest = "inp")
 parser.add_option("--output", action = "store", type = "string", dest = "out")
 parser.add_option("--scale", action = "store", type = "float", default = "1", dest = "scale")
 (options, args) = parser.parse_args()
-if not options.inp or not options.out or not options.scale:
+if not options.inp or not options.out:
     sys.exit("ERROR: Specify input, option, and scale")
 
 
@@ -23,8 +23,9 @@ scale = options.scale
 current_dir = os.getcwd()
 
 # Make directory
-subprocess.Popen("mkdir -p {}".format(
+p = subprocess.Popen("mkdir -p {}".format(
             options.out), shell=True, stdout=subprocess.PIPE)
+p.wait()
 
 # Read every file in the input directory, 
 dir_contents = os.listdir("{}".format(input_dir))
