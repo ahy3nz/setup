cf1ee13eb2a7b1dc437db706a87147bd3da3f953
ahy3nz <alexander.h.yang@vanderbilt.edu>
2017-07-20 17:30:41 -0500

    Fix file reading for reading LJ params

--- FF/CG/make_Tables.py
+++ FF/CG/make_Tables.py
@@ -4,12 +4,13 @@ import numpy as np
 import sys
 import subprocess
 
-filename = 'martini_pair.txt'
+filename = 'martini_pair.dat'
 #filename='testpair.txt'
 martini_file = open(filename,'r')
 martini_lines = martini_file.readlines()
 for i, line in enumerate(martini_lines):
-    split_lines = line.split()
+    split_lines = line.split(';')
+    split_lines = split_lines[0].split()
     # element 0 is first atom
     if(line.rstrip()):
         print(split_lines)
@@ -20,5 +21,6 @@ for i, line in enumerate(martini_lines):
         c6 = split_lines[3]
         # element 4 is c12term
         c12 = split_lines[4]
-        subprocess.Popen("python generate_Hoomd_Table.py -f {}-{} --c6 {} --c12 {}".format(
+        p = subprocess.Popen("python generate_Hoomd_Table.py -f {}-{} --c6 {} --c12 {}".format(
             atom_1, atom_2, c6, c12), shell=True, stdout=subprocess.PIPE)
+        p.wait()
