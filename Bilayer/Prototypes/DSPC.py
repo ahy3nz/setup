090b3df02e9081dc4e7ad923802a9985f7543c37
ahy3nz <alexander.h.yang@vanderbilt.edu>
2017-07-31 11:00:51 -0500

    Refactoring

--- Bilayer/Prototypes/DSPC.py
+++ Bilayer/Prototypes/DSPC.py
@@ -4,4 +4,5 @@ class DSPC(mb.Compound):
     def __init__(self):
         super(DSPC,self).__init__()
         mb.load(os.getcwd()+'/Prototypes/new_DSPC.gro', compound=self)
-        mb.translate(self, -self[0].pos)
+        #mb.translate(self, -self[0].pos)
+        self.translate(-self[0].pos)
