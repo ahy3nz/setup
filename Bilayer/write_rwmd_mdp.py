827b9459d7a927fa8b5c3f9c1582967985c19ee2
ahy3nz <alexander.h.yang@vanderbilt.edu>
2018-01-26 17:55:05 -0600

    Use metropolis algorithm for picking random temps

--- Bilayer/write_rwmd_mdp.py
+++ Bilayer/write_rwmd_mdp.py
@@ -1,4 +1,6 @@
 import os
+import pdb
+import random
 import sys
 import numpy as np
 
@@ -64,7 +66,7 @@ annealing                   = single single
 """.format(**locals()))
 
 def _generate_annealing_points(f, temp_low=305, temp_high=560, interval=5, 
-        time_anneal=25000, final_time=50000, water_temp=500):
+        time_anneal=25000, final_time=50000, water_temp=500, dtemp=20):
     """ Generate annealing points
     temp_low : lowest temperature, float
     temp_high : highest temperature, float
@@ -73,28 +75,60 @@ def _generate_annealing_points(f, temp_low=305, temp_high=560, interval=5,
     final_time : last time point such that the RWMD gradually
         brings temperature down to temp_low at final_time, int (ps)
     water_temp : temperature to keep water at, K 
+    dtemp : temperature intervals, (K)
     """
     n_points = int(time_anneal/interval)
     times = [0]
     temps = [temp_low]
     last_temp = temp_low
+    possible_temps = np.arange(temp_low, temp_high+1, dtemp)
+    # Construct the histogram
+    histogram = {val: 0 for val in possible_temps}
+    histogram[last_temp] += 1
     for i in np.arange(1,n_points):
-        # If we're at the lowest temperature, only go up
-        if abs(last_temp - temp_low) < 0.1:
-            dtemp = 5 * np.random.randint(0,2)
-        # If we're at the highest temperature, only go down
-        elif abs(last_temp - temp_high) < 0.1:
-            dtemp = 5 * np.random.randint(-1,1)
-        else:
-            dtemp = 5 * np.random.randint(-1,2)
+        # new stuff here
         time = i*interval
-        new_temp = last_temp + dtemp
         times.append(time)
+
+        # Randomly pick a temperature
+        candidate_temp = random.choice(possible_temps)
+
+        # Compare it to a criteria
+        if histogram[last_temp] >= histogram[candidate_temp]:
+            new_temp = candidate_temp
+        else:
+            new_temp = last_temp
+
+        histogram[new_temp] += 1
         temps.append(new_temp)
         last_temp = new_temp
 
+        ## ---old suf---
+        ## If we're at the lowest temperature, only go up
+        #if abs(last_temp - temp_low) < 0.1:
+        #    temp_change = dtemp * np.random.randint(0,2)
+        ## If we're at the highest temperature, only go down
+        #elif abs(last_temp - temp_high) < 0.1:
+        #    temp_change = dtemp * np.random.randint(-1,1)
+        #else:
+        #    temp_change = dtemp * np.random.randint(-1,2)
+        #time = i*interval
+        #new_temp = last_temp + temp_change
+        #times.append(time)
+        #temps.append(new_temp)
+        #last_temp = new_temp
+        ## -----end---
+
+    ### Saving stuff for reference
+    #with open('dictionary.dat', 'w') as thing:
+    #    for k, v in histogram.items():
+    #        thing.write("{}\t{}\n".format(k,v))
+    #np.savetxt('timeseries.dat', np.column_stack((times, temps)))
+    #### 
+
     temp_string = " ".join([str(temp) for temp in temps])
     temp_string += " " + str(temp_low)
+
     time_string = " ".join([str(time) for time in times])
     time_string += " " + str(final_time)
     f.write("annealing-npoints\t={0} 2 \n".format(n_points+1))
@@ -103,6 +137,6 @@ def _generate_annealing_points(f, temp_low=305, temp_high=560, interval=5,
 
 if __name__ == "__main__":
     with open('RW.mdp','w') as f:
-        _write_body(f, ref_temp=570)
-        _generate_annealing_points(f, temp_low=570, temp_high=620, interval=5,
-                time_anneal=25000, final_time=50000, water_temp=500)
+        _write_body(f, ref_temp=305)
+        _generate_annealing_points(f, temp_low=305, temp_high=585, interval=5,
+                time_anneal=25000, final_time=50000, water_temp=305, dtemp=28)
