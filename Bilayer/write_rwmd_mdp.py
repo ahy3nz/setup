4980ca3489668f9bda4b8967fca5d3bd0d3f0116
ahy3nz <alexander.h.yang@vanderbilt.edu>
2018-02-05 16:11:36 -0600

    Include different styles for thermostatting

--- Bilayer/write_rwmd_mdp.py
+++ Bilayer/write_rwmd_mdp.py
@@ -65,23 +65,40 @@ gen_vel                     = no
 annealing                   = single single
 """.format(**locals()))
 
-def _generate_annealing_points(f, temp_low=305, temp_high=560, interval=5, 
-        time_anneal=25000, final_time=50000, water_temp=500, dtemp=20):
+def _generate_annealing_points(f, nonw_temp_low=305, nonw_temp_high=560, interval=5, 
+        time_anneal=25000, final_time=50000, w_temp_low=500, w_temp_high=500,
+        dtemp=20,
+        water_thermostat_style=None):
     """ Generate annealing points
-    temp_low : lowest temperature, float
-    temp_high : highest temperature, float
+    nonw_temp_low : lowest temperature, float
+    nonw_temp_high : highest temperature, float
     interval : duration for each annealing interval, int (ps)
     time_anneal: duration of annealing, int (ps) 
     final_time : last time point such that the RWMD gradually
-        brings temperature down to temp_low at final_time, int (ps)
-    water_temp : temperature to keep water at, K 
+        brings temperature down to nonw_temp_low at final_time, int (ps)
+    w_temp_low : temperature to keep water at, K 
+    w_temp_high : temperature to keep water at, K 
     dtemp : temperature intervals, (K)
+    water_thermostat_style : str
+        Specifies how to thermstat water 
+        'plateau' for water to plateau if thermostat exceeds a temperature
+        'proportional' for water temp changes to be proportional to lipid temp
+        'constant' for water to maintain a single temperature
     """
     n_points = int(time_anneal/interval)
     times = [0]
-    temps = [temp_low]
-    last_temp = temp_low
-    possible_temps = np.arange(temp_low, temp_high+1, dtemp)
+    temps = [nonw_temp_low]
+    if water_thermostat_style == 'proportional':
+        w_temps= [w_temp_low]
+        last_w_temp = w_temp_low
+        scaling_factor = (w_temp_high - w_temp_low)/(nonw_temp_high - nonw_temp_low)
+    if water_thermostat_style == 'plateau':
+        w_temps = [w_temp_low]
+        last_w_temp = w_temp_low
+
+
+    last_temp = nonw_temp_low
+    possible_temps = np.arange(nonw_temp_low, nonw_temp_high+1, dtemp)
     # Construct the histogram
     histogram = {val: 0 for val in possible_temps}
     histogram[last_temp] += 1
@@ -96,28 +113,34 @@ def _generate_annealing_points(f, temp_low=305, temp_high=560, interval=5,
         # Compare it to a criteria
         if histogram[last_temp] >= histogram[candidate_temp]:
             new_temp = candidate_temp
+            if water_thermostat_style == 'proportional':
+                nonw_temp_change = new_temp - nonw_temp_low
+                w_temp_change = scaling_factor * nonw_temp_change 
+                new_w_temp = w_temp_low + w_temp_change
+            if water_thermostat_style == 'plateau':
+                if candidate_temp > w_temp_high:
+                    new_w_temp = w_temp_high
+                else:
+                    new_w_temp = candidate_temp
+
         else:
             new_temp = last_temp
+            if water_thermostat_style == 'proportional':
+                new_w_temp = last_w_temp
+            if water_thermostat_style == 'plateau':
+                new_w_temp = last_w_temp
 
         histogram[new_temp] += 1
         temps.append(new_temp)
+        if water_thermostat_style == 'proportional':
+            w_temps.append(new_w_temp)
+            last_w_temp = new_w_temp
+        if water_thermostat_style == 'plateau':
+            w_temps.append(new_w_temp)
+            last_w_temp = new_w_temp
+
         last_temp = new_temp
 
-        ## ---old suf---
-        ## If we're at the lowest temperature, only go up
-        #if abs(last_temp - temp_low) < 0.1:
-        #    temp_change = dtemp * np.random.randint(0,2)
-        ## If we're at the highest temperature, only go down
-        #elif abs(last_temp - temp_high) < 0.1:
-        #    temp_change = dtemp * np.random.randint(-1,1)
-        #else:
-        #    temp_change = dtemp * np.random.randint(-1,2)
-        #time = i*interval
-        #new_temp = last_temp + temp_change
-        #times.append(time)
-        #temps.append(new_temp)
-        #last_temp = new_temp
-        ## -----end---
 
     ### Saving stuff for reference
     #with open('dictionary.dat', 'w') as thing:
@@ -127,16 +150,33 @@ def _generate_annealing_points(f, temp_low=305, temp_high=560, interval=5,
     #### 
 
     temp_string = " ".join([str(temp) for temp in temps])
-    temp_string += " " + str(temp_low)
+    temp_string += " " + str(nonw_temp_low)
 
     time_string = " ".join([str(time) for time in times])
     time_string += " " + str(final_time)
-    f.write("annealing-npoints\t={0} 2 \n".format(n_points+1))
-    f.write("annealing-time\t={0} 0 {1}\n".format(time_string, str(final_time)))
-    f.write("annealing-temp\t={0} {1} {1}\n".format(temp_string, str(water_temp)))
+
+    if water_thermostat_style == 'proportional':
+        w_temp_string = " ".join([str(temp) for temp in w_temps])
+        w_temp_string += " " + str(w_temp_low)
+        f.write("annealing-npoints\t={0} {0}\n".format(n_points+1))
+        f.write("annealing-time\t={0} {0}\n".format(time_string))
+        f.write("annealing-temp\t={0} {1}\n".format(temp_string, w_temp_string))
+    if water_thermostat_style == 'plateau':
+        w_temp_string = " ".join([str(temp) for temp in w_temps])
+        w_temp_string += " " + str(w_temp_low)
+        f.write("annealing-npoints\t={0} {0}\n".format(n_points+1))
+        f.write("annealing-time\t={0} {0}\n".format(time_string))
+        f.write("annealing-temp\t={0} {1}\n".format(temp_string, w_temp_string))
+
+
+    if water_thermostat_style == 'constant':
+        f.write("annealing-npoints\t={0} 2 \n".format(n_points+1))
+        f.write("annealing-time\t={0} 0 {1}\n".format(time_string, str(final_time)))
+        f.write("annealing-temp\t={0} {1} {1}\n".format(temp_string, str(water_temp)))
 
 if __name__ == "__main__":
     with open('RW.mdp','w') as f:
         _write_body(f, ref_temp=305)
-        _generate_annealing_points(f, temp_low=305, temp_high=585, interval=5,
-                time_anneal=25000, final_time=50000, water_temp=305, dtemp=28)
+        _generate_annealing_points(f, nonw_temp_low=305, nonw_temp_high=705, interval=5,
+                time_anneal=25000, final_time=50000, w_temp_low=305, 
+                w_temp_high=505, dtemp=40, water_thermostat_style='plateau')
