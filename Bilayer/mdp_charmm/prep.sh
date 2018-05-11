a40fe516e586fb9d6f967b628c2cf7a2020e2960
ahy3nz <alexander.h.yang@vanderbilt.edu>
2018-05-11 10:19:06 -0500

    Fix flag passing

--- Bilayer/mdp_charmm/prep.sh
+++ Bilayer/mdp_charmm/prep.sh
@@ -6,7 +6,7 @@ gmx mdrun -deffnm em
 gmx grompp -f nvt.mdp -c em.gro -p $1.top -o nvt -maxwarn 2 &> nvt_grompp.log
 gmx mdrun -deffnm nvt
 
-gmx grompp -f npt_500ps.mdp -c nvt.gro -p $1.top -o npt -t nvt.cpt -maxwarn 2 &> npt_500ps_grompp.log
+gmx grompp -f npt_500ps.mdp -c nvt.gro -p $1.top -o npt_500ps -t nvt.cpt -maxwarn 2 &> npt_500ps_grompp.log
 gmx mdrun -deffnm npt_500ps
 
 gmx grompp -f npt.mdp -c npt_500ps.gro -p $1.top -o npt -t npt_500ps.cpt -maxwarn 2 &> npt_grompp.log
